9b089e501007768df3f1ec1c3bfcd47929ce30dc
M-MUGUNTHAN <52280716+M-MUGUNTHAN@users.noreply.github.com>
2020-01-19 08:38:06 +0530

    commit

--- newsapptest/App.js
+++ newsapptest/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Router from './components/Router';
 import {Provider} from 'react-redux';
 import { View, Text } from 'react-native';
-
+import store from './components/reduxUtils';
 const App = () => {
   return (
     <Provider store={store}>
